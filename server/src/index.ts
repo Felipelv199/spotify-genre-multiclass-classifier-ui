b9463954d8abbae091ab6f7bcae779dc91fa4d26
Felipelv199 <felipelopezv3@gmail.com>
2023-11-06 21:32:04 +0100

    Extract client and delete server

--- server/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { config } from 'dotenv';
-import app from './app';
-
-config({ path: '.env.local' });
-const port = process.env.PORT;
-app.listen(port, () => console.log(`Listening on port ${port}`));
